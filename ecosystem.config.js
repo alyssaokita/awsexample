353ccc6b49b2e0c2a8a6ab51db51b8aaa7b0e8e4
Robert Baker <robert@applariat.com>
2018-12-12 06:29:10 -0800

    Add pm2 deployment config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'Development',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-224-228-174.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:rbaker2016/awsexample.git',
+      path: '/home/ubuntu/awsexample',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
