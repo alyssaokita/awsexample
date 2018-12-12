7bb19b4cc8971289a9f9b147451936b551df44f8
Robert Baker <robert@applariat.com>
2018-12-12 06:50:48 -0800

    serve static content

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY DUDE!')
-})
+
+app.use(express.static('public'))
+// app.get('/', (req, res) => {
+//   res.send('HEY DUDE!')
+// })
 app.listen(3000, () => console.log('Server running on port 3000'))
