9a0ce242b428770a5d7bb959bab550a26000717e
Robert Baker <robert@applariat.com>
2019-01-23 06:12:02 -0800

    Change repo username

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host: 'ec2-18-224-228-174.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/development_key.pem',
       ref: 'origin/master',
-      repo: 'git@github.com:rbaker2016/awsexample.git',
+      repo: 'git@github.com:rbakerdev/awsexample.git',
       path: '/home/ubuntu/awsexample',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const express = require('express')
 const app = express()
+const express = require('express')
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
 
 app.use(express.static('public'))
 // app.get('/', (req, res) => {
 //   res.send('HEY DUDE!')
 // })
-app.listen(3000, () => console.log('Server running on port 3000'))
+
+// TODO: parameterize locations and ports to env
+const KEY_PATH = '/etc/letsencrypt/live/bakercodelife.com/privkey.pem';
+const CERT_PATH = '/etc/letsencrypt/live/bakercodelife.com/cert.pem';
+const options = {
+    key: fs.readFileSync(KEY_PATH, 'utf-8'),
+    cert: fs.readFileSync(CERT_PATH, 'utf-8')
+}
+
+const httpsPort = 433
+const httpPort = 80
+const httpsServer = https.createServer(options, app)
+const httpServer = http.createServer(app)
+
+httpsServer.listen(httpsPort, () => {
+    console.log(`HTTPS listening on port ${httpsPort}`)
+})
+
+httpServer.listen(httpPort, () => {
+    console.log(`HTTP listening on port ${httpPort}`)
+})
