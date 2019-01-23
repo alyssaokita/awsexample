e4502a17b6548c054610c2dcb33c04d5a9dc3237
Robert Baker <robert@applariat.com>
2019-01-23 08:06:32 -0800

    Disable https in server

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ app.use(express.static('public'))
 // })
 
 // TODO: parameterize locations and ports to env
-const KEY_PATH = '/etc/letsencrypt/live/bakercodelife.com/privkey.pem';
-const CERT_PATH = '/etc/letsencrypt/live/bakercodelife.com/cert.pem';
-const options = {
-    key: fs.readFileSync(KEY_PATH, 'utf-8'),
-    cert: fs.readFileSync(CERT_PATH, 'utf-8')
-}
+// const KEY_PATH = '/etc/letsencrypt/live/bakercodelife.com/privkey.pem';
+// const CERT_PATH = '/etc/letsencrypt/live/bakercodelife.com/cert.pem';
+// const options = {
+//     key: fs.readFileSync(KEY_PATH, 'utf-8'),
+//     cert: fs.readFileSync(CERT_PATH, 'utf-8')
+// }
 
-const httpsPort = 433
+// const httpsPort = 433
 const httpPort = 80
-const httpsServer = https.createServer(options, app)
+// const httpsServer = https.createServer(options, app)
 const httpServer = http.createServer(app)
 
-httpsServer.listen(httpsPort, () => {
-    console.log(`HTTPS listening on port ${httpsPort}`)
-})
+// httpsServer.listen(httpsPort, () => {
+//     console.log(`HTTPS listening on port ${httpsPort}`)
+// })
 
 httpServer.listen(httpPort, () => {
     console.log(`HTTP listening on port ${httpPort}`)
